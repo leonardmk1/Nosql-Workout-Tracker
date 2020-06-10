29ef30f0b537ef381a7186c3d9b487215162d866
Matthew Leonard <leonard.matthewk@gmail.com>
2020-06-10 16:55:05 -0400

    added server.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,14 +2,23 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-      .then(result => {
-        res.json(result);
-      })
-      .catch(err => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("api/workouts", (req, res) => {
+    Workout.find()
+    .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    })
+    .catch((err) => {
         res.json(err);
-      });
-  });
+      });  
+})
 
-
-  module.exports = router;
+module.exports = router;
