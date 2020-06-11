409b997765dc9e01378e50e5970235e42f32bc45
Matthew Leonard <leonard.matthewk@gmail.com>
2020-06-11 07:00:01 -0400

    trying to fix apiRoutes

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -38,19 +38,8 @@ const workoutSchema = new Schema(
       },
     },
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
+  
 );
-
-workoutSchema.virtual("totalDuration").get(function() {
-    // "reduce" array of exercises down to just the sum of their durations
-    return this.exercises.reduce((total, exercise) => {
-      return total + exercise.duration;
-    }, 0);
-  });
 
 
 const Workout = mongoose.model("Workout", workoutSchema);
