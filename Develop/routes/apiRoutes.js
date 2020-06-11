48cb4a649a8f10c91035051eed504a07500a455f
Matthew Leonard <leonard.matthewk@gmail.com>
2020-06-11 18:25:15 -0400

    working to get api routes working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,44 +1,36 @@
-// const router = require("express").Router();
+const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// router.post("/api/workouts", (req, res) => {
-//     console.log(req)
-//     console.log("hit")
+router.post("/api/workouts", (req, res) => {
+    console.log(req)
+    console.log("hit")
 
-//   Workout.create({
-//     date: Date.now,
-//     exercise:{ 
-//     type: "cardio",
-//     name: running,
-//     distance: 1,
-//     duration: 15}
-//   })
-//     .then((dbWorkout) => {
-//       res.json(dbWorkout);
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     });
-// });
-
-// router.get("api/workouts", (req, res) => {
-//     console.log("hit")
-//     Workout.find()
-//     .then((dbWorkout) => {
-//         console.log(dbWorkout)
-//         res.json(dbWorkout);
-//     })
-//     .catch((err) => {
-//         res.json(err);
-//       });  
-// })
-
-// module.exports = router;
-
-module.exports = function(app) {
-    app.get("/api/workout", function(req, res) {
-      db.Workout.find({}).then(function(dbWorkout) {
-        res.json(dbWorkout);
-      });
+  Workout.create({
+    // date: Date.now,
+    // exercise:{ 
+    // type: "cardio",
+    // name: running,
+    // distance: 1,
+    // duration: 15}
+  })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
-}
+});
+
+router.get("api/workouts", (req, res) => {
+    console.log("hit")
+    Workout.find()
+    .then((dbWorkout) => {
+        console.log(dbWorkout)
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+      });  
+})
+
+module.exports = router;
