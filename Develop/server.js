15e2de8d1d2907108e99ddde7bdf6b4bd87b421f
Matthew Leonard <leonard.matthewk@gmail.com>
2020-06-11 23:52:05 -0400

    update

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // db mongo
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds131687.mlab.com:31687/heroku_lrmzj00b", {
   useNewUrlParser: true,
   useFindAndModify: false
 }).then(function(){
